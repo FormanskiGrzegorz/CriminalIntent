602fd6c4ab76e1ebd705a4ea3bd4a56c02f761dd
Grzegorz Fórmański <grzegorz.formanski@gmail.com>
2019-02-18 19:07:07 +0100

    commit 14

--- app/src/main/java/bignerdranch/android/criminalintent/CrimeLab.java
+++ app/src/main/java/bignerdranch/android/criminalintent/CrimeLab.java
@@ -19,12 +19,10 @@ public class CrimeLab {
 
     private CrimeLab(Context context) {
         mCrimes = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            Crime crime = new Crime();
-            crime.setTitle("Sprawa #" + i);
-            crime.setSolved(i % 2 == 0); //co drugi obiekt
-            mCrimes.add(crime);
-        }
+    }
+
+    public void addCrime(Crime c) {
+        mCrimes.add(c);
     }
 
     public List<Crime> getCrimes() {
